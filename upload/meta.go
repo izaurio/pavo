990587b82755b579e0f896dce6418334a2170c18
Zaur Abasmirzoev <zaur.kavkaz@gmail.com>
2014-08-29 11:13:07 +0400

    Upload meta parser

---
+++ upload/meta.go
@@ -0,0 +1,104 @@
+package upload
+
+import (
+	"errors"
+	"fmt"
+	"mime"
+	"net/http"
+)
+
+// Info about request headers
+type Meta struct {
+	MediaType string
+	Boundary  string
+	Range     *Range
+	Filename  string
+}
+
+type Range struct {
+	Start int64
+	End   int64
+	Size  int64
+}
+
+// Parse request headers and make Meta.
+func ParseMeta(req *http.Request) (*Meta, error) {
+	meta := &Meta{}
+
+	if err := meta.parseContentType(req.Header.Get("Content-Type")); err != nil {
+		return nil, err
+	}
+
+	if err := meta.parseContentRange(req.Header.Get("Content-Range")); err != nil {
+		return nil, err
+	}
+
+	if err := meta.parseContentDisposition(req.Header.Get("Content-Disposition")); err != nil {
+		return nil, err
+	}
+
+	return meta, nil
+}
+
+func (meta *Meta) parseContentType(ct string) error {
+	if ct == "" {
+		meta.MediaType = "application/octet-stream"
+		return nil
+	}
+
+	mediatype, params, err := mime.ParseMediaType(ct)
+	if err != nil {
+		return err
+	}
+
+	if mediatype == "multipart/form-data" {
+		boundary, ok := params["boundary"]
+		if !ok {
+			return errors.New("meta: boundary not defined")
+		}
+
+		meta.MediaType = mediatype
+		meta.Boundary = boundary
+	} else {
+		meta.MediaType = "application/octet-stream"
+	}
+
+	return nil
+}
+
+func (meta *Meta) parseContentRange(cr string) error {
+	if cr == "" {
+		return nil
+	}
+
+	var start, end, size int64
+
+	_, err := fmt.Sscanf(cr, "bytes %d-%d/%d", &start, &end, &size)
+	if err != nil {
+		return err
+	}
+
+	meta.Range = &Range{Start: start, End: end, Size: size}
+
+	return nil
+}
+
+func (meta *Meta) parseContentDisposition(cd string) error {
+	if cd == "" {
+		return nil
+	}
+
+	_, params, err := mime.ParseMediaType(cd)
+	if err != nil {
+		return err
+	}
+
+	filename, ok := params["filename"]
+	if !ok {
+		return errors.New("meta: filename in Content-Disposition not defined")
+	}
+
+	meta.Filename = filename
+
+	return nil
+}
---
+++ upload/meta_test.go
@@ -0,0 +1,29 @@
+package upload
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseMeta(t *testing.T) {
+	assert := assert.New(t)
+
+	req, _ := http.NewRequest("POST", "/files", nil)
+	req.Header.Set("Content-Type", "multipart/form-data; boundary=----Zam1WUeLK7vBj4wN")
+	req.Header.Set("Content-Range", "bytes 512000-1023999/1141216")
+	req.Header.Set("Content-Disposition", `attachment; filename="picture.jpg"`)
+
+	meta, err := ParseMeta(req)
+	assert.Nil(err)
+
+	assert.Equal(meta.MediaType, "multipart/form-data")
+	assert.Equal(meta.Boundary, "----Zam1WUeLK7vBj4wN")
+
+	assert.Equal(meta.Range.Start, 512000)
+	assert.Equal(meta.Range.End, 1023999)
+	assert.Equal(meta.Range.Size, 1141216)
+
+	assert.Equal(meta.Filename, "picture.jpg")
+}
