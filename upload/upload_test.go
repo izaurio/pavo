920ff981e6c79393b46bfaa7f4eb4c5217bcccb2
Zaur Abasmirzoev <zaur.kavkaz@gmail.com>
2014-08-29 13:38:31 +0400

    begin upload test

--- upload/upload_test.go
+++ upload/upload_test.go
@@ -1,1 +1,50 @@
 package upload
+
+import (
+	"bytes"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUploadMultipart(t *testing.T) {
+	assert := assert.New(t)
+
+	var body bytes.Buffer
+	mw := multipart.NewWriter(&body)
+
+	if err := writeMPBody("../dummy/32509211_news_bigpic.jpg", mw); err != nil {
+		assert.Error(err)
+	}
+	if err := writeMPBody("../dummy/kino.jpg", mw); err != nil {
+		assert.Error(err)
+	}
+
+	mw.Close()
+
+	req, _ := http.NewRequest("POST", "/files", &body)
+	req.Header.Set("Content-Type", mw.FormDataContentType())
+
+	t.Logf("%#v", req.Header)
+
+}
+
+func writeMPBody(fname string, mw *multipart.Writer) error {
+	fw, _ := mw.CreateFormFile("files[]", filepath.Base(fname))
+	f, err := os.Open(fname)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(fw, f)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
